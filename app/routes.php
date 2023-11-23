3d570aab7c066a2dda842627ce927715a450cf2c
programarivm <info@programarivm.com>
2023-11-23 15:51:40 +0100

    Rendered the about page

--- app/routes.php
+++ app/routes.php
@@ -8,6 +8,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Slim\Views\PhpRenderer;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -23,5 +24,10 @@ return function (App $app) {
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
+    });
+
+    $app->get('/about', function (Request $request, Response $response) {
+        $renderer = new PhpRenderer('../templates');
+        return $renderer->render($response, "about.php");
     });
 };
