c05277bcd3cf84370687fa5b4deaf5d133146c7a
programarivm <info@programarivm.com>
2023-12-06 23:34:17 +0100

    Updated title

--- templates/games.html.php
+++ templates/games.html.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <link rel="icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ChesslaBlab — Games</title>
+    <title>ChessCoach — Games</title>
     <meta name="description" content="A data visualization analysis tool for chess games.">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/styles.css">
--- templates/openings.html.php
+++ templates/openings.html.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <link rel="icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ChesslaBlab — Chess Openings</title>
+    <title>ChessCoach — Chess Openings</title>
     <meta name="description" content="A: Flank Openings. B: Semi-Open Games other than the French Defense. C: Open Games and the French Defense. D: Closed Games and Semi-Closed Games. E: Indian Defenses.">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/styles.css">
