bd6d338089c4f989d26afb73a1f146b4206e0a23
programarivm <info@programarivm.com>
2023-12-15 11:29:31 +0100

    Added Open Graph and Twitter card tags

--- templates/home.html.php
+++ templates/home.html.php
@@ -4,8 +4,24 @@
     <meta charset="utf-8">
     <link rel="icon" href="/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ChessCoach — Games</title>
+    <title>ChessCoach — Chess Positions Explained</title>
     <meta name="description" content="ChessCoach describes a chess position both verbally and visually helping you improve your chess thinking process.">
+
+    <meta property="og:title" content="ChessCoach — Chess Positions Explained">
+    <meta property="og:description" content="ChessCoach describes a chess position both verbally and visually helping you improve your chess thinking process.">
+    <meta property="og:url" content="<?php echo $scheme; ?>://<?php echo $host; ?>:<?php echo $port; ?>">
+    <meta property="og:image" content="<?php echo $scheme; ?>://<?php echo $host; ?>:<?php echo $port; ?>/assets/img/cover.jpg">
+    <meta property="og:site_name" content="ChessCoach">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="article">
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="ChessCoach — Chess Positions Explained">
+    <meta name="twitter:description" content="ChessCoach describes a chess position both verbally and visually helping you improve your chess thinking process.">
+    <meta name="twitter:site" content="@programarivm">
+    <meta name="twitter:image" content="<?php echo $scheme; ?>://<?php echo $host; ?>:<?php echo $port; ?>/assets/img/cover.jpg">
+    <meta name="twitter:creator" content="@programarivm">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/styles.css">
     <link rel="stylesheet" href="/assets/css/spinner.css">
