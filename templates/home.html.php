703ba298da0352c52974a4d54ebb3b6468594abe
programarivm <info@programarivm.com>
2023-12-24 18:38:29 +0100

    Wording

--- templates/home.html.php
+++ templates/home.html.php
@@ -40,10 +40,10 @@
               Explains a chess position verbally and visually
             </p>
             <p>
-              Beginners often think they can checkmate the opponent's king quickly.
-              However, there are so many things to look at in order to understand
-              a position. ChessCoach helps you improve your chess thinking process.
-              Check it out for yourself right now!
+              Chess beginners often think they can checkmate the opponent's king
+              quickly. However, there are so many different things to consider in
+              order to understand a position. ChessCoach helps you improve your
+              chess thinking process. Check it out for yourself right now!
             </p>
             <form id="gameForm">
               <div class="form-group">
