3852c1a7be9d2c0d1cc6029e0d88f35ee4fb9fe6
programarivm <info@programarivm.com>
2023-12-28 12:32:10 +0100

    CSS minification

--- templates/moves.html.php
+++ templates/moves.html.php
@@ -23,7 +23,7 @@
     <meta name="twitter:creator" content="@programarivm">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="/assets/css/spinner.min.css">
     <link rel="stylesheet" href="/assets/css/styles.min.css">
   </head>
