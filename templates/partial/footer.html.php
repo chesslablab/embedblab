41ce50e330a1d04ddc7ac1be747f6915313c6892
programarivm <info@programarivm.com>
2024-01-10 18:14:00 +0100

    Updated footer.html.php

--- templates/partial/footer.html.php
+++ templates/partial/footer.html.php
@@ -1,10 +1,7 @@
 <footer class="py-3 my-4">
   <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-    <li class="nav-item"><a href="/games" class="nav-link px-2">Games</a></li>
-    <li class="nav-item"><a href="/openings" class="nav-link px-2">Openings</a></li>
-    <li class="nav-item"><a href="/positions" class="nav-link px-2">Positions</a></li>
-    <li class="nav-item"><a href="/moves" class="nav-link px-2">Moves</a></li>
-    <li class="nav-item"><a href="/heuristics" class="nav-link px-2">Heuristics</a></li>
+    <li class="nav-item"><a href="/chat" target="_blank" class="nav-link px-2">Chatroom</a></li>
+    <li class="nav-item"><a href="/about" class="nav-link px-2">About Us</a></li>
   </ul>
   <p class="text-center text-muted">&copy; 2024 ChesslaBlab</p>
 </footer>
