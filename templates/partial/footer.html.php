516af83701ad239f67929f3237e80be322043dd9
programarivm <info@programarivm.com>
2024-01-10 16:59:43 +0100

    Updated footer.html.php

--- templates/partial/footer.html.php
+++ templates/partial/footer.html.php
@@ -1,10 +1,11 @@
-<footer class="py-5">
-  <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-    <p>&copy; 2023 ChesslaBlab. All rights reserved.</p>
-    <ul class="list-unstyled d-flex">
-      <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
-      <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
-      <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
-    </ul>
-  </div>
+<footer class="py-3 my-4">
+  <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+    <li class="nav-item"><a href="/about" class="nav-link px-2">About</a></li>
+    <li class="nav-item"><a href="/games" class="nav-link px-2">Games</a></li>
+    <li class="nav-item"><a href="/openings" class="nav-link px-2">Openings</a></li>
+    <li class="nav-item"><a href="/positions" class="nav-link px-2">Positions</a></li>
+    <li class="nav-item"><a href="/moves" class="nav-link px-2">Moves</a></li>
+    <li class="nav-item"><a href="/heuristics" class="nav-link px-2">Heuristics</a></li>
+  </ul>
+  <p class="text-center text-muted">&copy; 2024 ChesslaBlab</p>
 </footer>
