7ab495efc72b7d09bdeedc68482fe276e11193c3
programarivm <info@programarivm.com>
2024-01-15 20:02:25 +0100

    Updated nav.html.php

--- templates/partial/nav.html.php
+++ templates/partial/nav.html.php
@@ -7,6 +7,9 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/games">Games</a>
         </li>
         <li class="nav-item">
